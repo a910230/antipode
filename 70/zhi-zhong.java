c4c58d2031a4e89b8ed9d60a14fe377874e3ab4c
a910230 <56457076+a910230@users.noreply.github.com>
2023-06-21 00:45:18 -0500

    Update zhi-zhong.java

--- 70/zhi-zhong.java
+++ 70/zhi-zhong.java
@@ -4,13 +4,9 @@ class Solution {
         ways[0] = ways[1] = 1;
         
         for (int i = 2; i < n + 1; ++i) {
-            ways[i] = climb(i, ways);
+            ways[i] = ways[i - 1] + ways[i - 2];
         }
         
         return ways[n];
-    }
-    
-    private int climb(int n, int[] ways)  {
-        return ways[n - 1] + ways[n - 2];
     }
 }
