e22c326bd39e8341e1f32bdb5126fa2cccef9a88
a910230 <56457076+a910230@users.noreply.github.com>
2023-06-26 21:42:49 -0500

    Update zhi-zhong.java

--- 11/zhi-zhong.java
+++ 11/zhi-zhong.java
@@ -1,35 +1,18 @@
-// Optimization needed
-
 class Solution {
     public int maxArea(int[] height) {
-        Map<Integer, int[]> hMap = new TreeMap<>(Collections.reverseOrder());
-        for (int i = 0; i < height.length; ++i) {
-            if (hMap.containsKey(height[i])) hMap.get(height[i])[1] = i;
-            else {
-                int[] pos = new int[]{i, i};
-                hMap.put(height[i], pos);
-            }
+        int l = 0;
+        int r = height.length - 1;
+        int area = Math.min(height[l], height[r]) * (r - l);
+        
+        if (height[l] < height[r]) ++l;
+        else --r;
+        
+        while (l < r) {
+            area = Math.max(area, Math.min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r]) ++l;
+            else --r;
         }
-        
-        int lPos = height.length;
-        int rPos = -1;
-        int lMost = lPos;
-        int rMost = rPos;
-        int area = -1;
-        
-        for (Map.Entry<Integer, int[]> entry: hMap.entrySet()) {
-            int[] pos = entry.getValue();
-            lMost = Math.min(lMost, pos[0]);
-            rMost = Math.max(rMost, pos[1]);
-            int curArea = (rMost - lMost) * entry.getKey();
-            
-            if (curArea > area) {
-                lPos = lMost;
-                rPos = rMost;
-                area = curArea;
-            }
-        }
-        
+                
         return area;
     }
 }
